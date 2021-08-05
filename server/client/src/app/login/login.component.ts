16b2866108d76995ebc5e3d8d8ad9b4efc610ff8
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2021-08-05 08:56:29 +0530

    Updated

---
+++ server/client/src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private toastr: ToastrService,private httpClient: HttpClient,private router:Router){
+
+  }
+  ngOnInit(): void {
+  }
+  showToast():void{
+    this.toastr.error('Enter all fields !');
+  }
+  createAccount():void{
+    this.router.navigateByUrl('/signup').then();
+  }
+  signHandler():void{
+    let email=(<HTMLInputElement>document.getElementById('email')).value;
+    let password=(<HTMLInputElement>document.getElementById('password')).value;
+    if (email.length ===0 || password.length ===0)
+    {
+      this.showToast();
+    }
+    else
+    {
+      const headers={'content-type':'application/json'};
+      const body={"email":email,"password":password};
+      const signurl='http://localhost:4201/login'
+      this.httpClient.post(signurl,body,{'headers':headers})
+      .subscribe(res=>{
+        localStorage.setItem('todolist',"logged In with email: "+email);
+        this.toastr.success('Logged In Successfully !');
+        this.router.navigateByUrl('/show-task').then();
+      },
+        err=>{
+          this.toastr.error('Invalid Email or Password !');
+        }
+      )
+    }
+  }
+}
