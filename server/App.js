ba0d9e4b306e445d90f913e69cee9f34853a5940
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2021-08-05 08:37:40 +0530

    Updated

--- server/App.js
+++ server/App.js
@@ -24,7 +24,7 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
         const path=require('path')
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname,'client','src','index.html'))
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
         })
         }
 app.listen(PORT,()=>{
