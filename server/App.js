b555571f947198be1651df80dbff853da7b6fe4c
Yaswanth <65965757+Yaswanth-L@users.noreply.github.com>
2021-08-05 10:24:44 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -24,7 +24,7 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static('public'))
         const path=require('path')
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname,'public/index.html'))
+            res.sendFile(path.resolve(__dirname,'/client/server/public/index.html'))
         })
         }
 app.listen(PORT,()=>{
